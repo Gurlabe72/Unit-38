b19fa3a631e96e0b805b1d983f05febb4700c32f
Gurlabe Dhaliwal <GurlabeD@Ramans-MacBook-Air.local>
2018-09-09 14:55:54 -0700

    2nd commit, buttons are not in 2x2 form, however they are color coded. also my event listening fucntion is not recognizing ('red-button').click

---
+++ pixel-art-maker-master/main.js
@@ -0,0 +1,29 @@
+function afterRedButtonClicked(event){
+    var bodyElement = $(body);
+    // This changes the background-color of the body to red
+    bodyElement.css('background-color', 'red');  
+}
+('.red-button').click(afterRedButtonClicked);
+
+function afterGreenButtonClicked(event){
+    /* Fill in this listener */
+    var bodyElement= $('body')
+    bodyElement.css('background-color', 'green')
+  } 
+  ('.green-button').click(afterGreenButtonClicked);
+
+function afterBlueButtonClicked(event){
+    /* Fill in this listener */
+    var bodyElement= $('body')
+    bodyElement.css('background-color', 'blue')
+  } 
+  ('.blue-button').click(afterBlueButtonClicked);
+
+  
+
+function afterOrangeButtonClicked(event){
+    /*  Fill in this listener */
+    var bodyElement= $('body')
+    bodyElement.css('background-color', 'orange')
+  } 
+  ('.orange-button').click(afterOrangeButtonClicked);
